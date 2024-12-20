aea1dec7a606414f28683368b45c51f04d0036ab
Nikolas Sturaro <nikolas.sturaro@cherrybank.it>
2024-12-21 00:11:54 +0100

    lang_chain - ice_breaker - test with gpt-4o

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,14 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
+information = """
+Elon Reeve Musk (/ˈiːlɒn/; born June 28, 1971) is a businessman known for his key roles in the space company SpaceX and the automotive company Tesla, Inc. His other involvements include ownership of X Corp., the company that operates the social media platform X (formerly Twitter), and his role in the founding of the Boring Company, xAI, Neuralink, and OpenAI. Musk is the wealthiest individual in the world; as of December 2024, Forbes estimates his net worth to be US$439.4 billion.[2]
+
+A member of the wealthy South African Musk family, Musk was born in Pretoria and briefly attended the University of Pretoria before immigrating to Canada at the age of 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University but never enrolled in classes, and with his brother Kimbal co-founded the online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same year, Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In 2002, Musk acquired US citizenship, and that October eBay acquired PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight services company, in 2002.
+"""
 if __name__ == "__main__":
     # load_dotenv()
     print("Hello LangChain!")
@@ -12,3 +19,13 @@ if __name__ == "__main__":
         1. a short summary
         2. two interesting facts about them
     """
+
+    summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key=os.environ.get("OPENAI_API_KEY"))
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": information})
+
+    print(res)
