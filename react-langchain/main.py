3adc3c7815ea93035e613459b6fbca9b621e3a9a
Nikolas Sturaro <nikolas.sturaro@gmail.com>
2025-03-02 18:38:25 +0100

    lang_chain - react-langchain - ADD LLM callback

--- react-langchain/main.py
+++ react-langchain/main.py
@@ -1,11 +1,14 @@
-from typing import Union
+from typing import Union, List
 
 from dotenv import load_dotenv
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import tool, render_text_description
+from langchain_core.tools import tool, render_text_description, Tool
 from langchain_openai import ChatOpenAI
+
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
 
@@ -16,6 +19,13 @@ def get_text_length(text: str) -> int:
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip('"')  # stripping away non alphabetic characters just in case
     return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str):
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found in tools")
 
 
 if __name__ == "__main__":
@@ -34,27 +44,46 @@ if __name__ == "__main__":
     Action Input: the input to the action
     Observation: the result of the action
     ... (this Thought/Action/Action Input/Observation can repeat N times)
-    
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
     Begin!
     
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
 
-
-## Thought: I now know the final answer
-## Final Answer: the final answer to the original input question
+    ## Thought: I now know the final answer
+    ## Final Answer: the final answer to the original input question
     prompt = PromptTemplate.from_template(template=template).partial(tools=render_text_description(tools),
                                                                      tool_names=", ".join(t.name for t in tools))
 
-    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
+    llm = ChatOpenAI(temperature=0, stop=["\n    Observation"], callbacks=[AgentCallbackHandler()])
+    intermediate_steps = []
 
-    agent = {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+    agent = {"input": lambda x: x["input"],
+             "agent_scratchpad": lambda x: format_log_to_str(
+                 x["agent_scratchpad"])} | prompt | llm | ReActSingleInputOutputParser()
 
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of 'DOG' in characters?"})
+    n_iterations: int = 1
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
 
-    print(agent_step)
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {"input": "What is the length in characters of the text 'DOG' ?",
+             "agent_scratchpad": intermediate_steps})
 
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        ##tool_to_use = find_tool_by_name(tools, tools)
+        print(f"agent_step-{n_iterations}: {agent_step}")
+
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation}")
+            intermediate_steps.append((agent_step, str(observation)))
+            n_iterations += 1
+
+    if isinstance(agent_step, AgentFinish):
+        print(f"Final Answer: {agent_step.return_values}")
